0c78b4829fe0b712de70529d4f726645e81d2cc4
gybran nauval <nauvalgybran@gmail.com>
2025-05-29 22:58:22 +0700

    fix(main): override HTTP client to accept bad certificates and handle errors with runZonedGuarded

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
+import 'dart:async';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:emonesia/routes/app_routes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+class MyHttpoverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() async {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    HttpOverrides.global = MyHttpoverrides();
+    runApp(const MyApp());
+  }, (error, stack) {
+    log('Error: $error');
+    log('Stack trace: $stack');
+  });
+  ;
 }
 
 class MyApp extends StatelessWidget {
@@ -22,6 +44,11 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Inter',
       ),
       getPages: AppRoutes.routes,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
     );
   }
 }
