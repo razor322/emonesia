e710c581a3a780e391751947ade22cf03cc37cb0
gybran nauval <nauvalgybran@gmail.com>
2025-05-31 23:15:59 +0700

    imp(text-field): add maxLength enforcement and focus node

--- lib/components/input/custom_boerderless_text_field.dart
+++ lib/components/input/custom_boerderless_text_field.dart
@@ -1,6 +1,7 @@
 import 'package:emonesia/styles/app_colors.dart';
 import 'package:emonesia/styles/app_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomBorderlessTextField extends StatelessWidget {
   final TextEditingController? controller;
@@ -24,6 +25,7 @@ class CustomBorderlessTextField extends StatelessWidget {
   final bool enabled;
   final bool? obscureText;
   final Function()? onTap;
+  final FocusNode? focusNode;
 
   const CustomBorderlessTextField({
     Key? key,
@@ -52,94 +54,107 @@ class CustomBorderlessTextField extends StatelessWidget {
     this.enabled = true,
     this.obscureText = false,
     this.onTap,
+    this.focusNode,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: AppSizes.s280,
-      height: AppSizes.s50,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-        border: Border.all(width: 2, color: AppColors.colorBorder),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.colorPrimary.withOpacity(0.25),
-            spreadRadius: AppSizes.s11,
-            blurRadius: AppSizes.s20,
-            offset: Offset(AppSizes.s0, AppSizes.s4),
-          ),
-        ],
-      ),
-      child: TextFormField(
-        onChanged: onChanged,
-        showCursor: true,
-        controller: controller,
-        keyboardType: TextInputType.text,
-        maxLines: 30,
-        maxLength: maxLength,
-        obscureText: obscureText ?? false,
-        onFieldSubmitted: onSubmitted,
-        cursorColor: cursorColor ?? Colors.black45,
-        textAlign: textAlign ?? TextAlign.start,
-        validator: (value) {
-          if (isOptional ?? false) {
-            return null;
-          }
-          if (value == null || value.isEmpty) {
-            return "Field ini wajib diisi";
-          }
-          return null;
-        },
-        style: TextStyle(
-            fontFamily: 'OpenSans',
-            color: textColor ?? Colors.black,
-            fontSize: textSize ?? FontSize.s14),
-        readOnly: readOnly,
-        enabled: enabled,
-        onTap: onTap,
-        decoration: InputDecoration(
-          suffixIcon: suffixIcon,
-          fillColor: fillColor ?? Colors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-            borderSide: const BorderSide(
-              color: Colors.transparent,
+        width: AppSizes.s280,
+        height: AppSizes.s50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+          border: Border.all(width: 2, color: AppColors.colorBorder),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.colorPrimary.withOpacity(0.25),
+              spreadRadius: AppSizes.s11,
+              blurRadius: AppSizes.s20,
+              offset: Offset(AppSizes.s0, AppSizes.s4),
             ),
-          ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          disabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-            borderSide: const BorderSide(
-              color: Colors.transparent,
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-            borderSide: const BorderSide(
-              color: Colors.transparent,
-            ),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-            borderSide: const BorderSide(
-              color: Colors.transparent,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
-            borderSide: const BorderSide(
-              color: Colors.transparent,
-            ),
-          ),
-          filled: true,
-          hintText: hintText,
-          hintStyle: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: hintSize ?? 16,
-              color: hintColor ?? Colors.grey),
+          ],
         ),
-      ),
-    );
+        child: TextFormField(
+          focusNode: focusNode,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'^[a-zA-Z0-9\s]+$')),
+          ],
+          onChanged: onChanged,
+          showCursor: true,
+          controller: controller,
+          keyboardType: TextInputType.text,
+          maxLines: 1,
+          maxLength: 30,
+          obscureText: obscureText ?? false,
+          onFieldSubmitted: onSubmitted,
+          cursorColor: cursorColor ?? Colors.black45,
+          textAlign: textAlign ?? TextAlign.start,
+          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+          buildCounter: (
+            _, {
+            required int currentLength,
+            required bool isFocused,
+            required int? maxLength,
+          }) =>
+              null,
+          validator: (value) {
+            if (isOptional ?? false) {
+              return null;
+            }
+            if (value == null || value.isEmpty) {
+              return "Field ini wajib diisi";
+            }
+            return null;
+          },
+          style: TextStyle(
+              fontFamily: 'OpenSans',
+              color: textColor ?? Colors.black,
+              fontSize: textSize ?? FontSize.s14),
+          readOnly: readOnly,
+          enabled: enabled,
+          onTap: onTap,
+          decoration: InputDecoration(
+            suffixIcon: suffixIcon,
+            fillColor: fillColor ?? Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            contentPadding:
+                EdgeInsets.only(left: AppSizes.s16, top: AppSizes.s20),
+            disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(radius ?? AppSizes.s4),
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+              ),
+            ),
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: hintSize ?? 16,
+                color: hintColor ?? Colors.grey),
+          ),
+        ));
   }
 }
