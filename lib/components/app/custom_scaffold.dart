e79d9246052ffb2247abdfe529fa75864ddb37a8
gybran nauval <nauvalgybran@gmail.com>
2025-05-31 23:11:55 +0700

    feat(scaffold): display app version in bottomSheet

--- lib/components/app/custom_scaffold.dart
+++ lib/components/app/custom_scaffold.dart
@@ -1,15 +1,27 @@
+import 'package:emonesia/resources/constants/app_constants.dart';
 import 'package:emonesia/resources/constants/asset_constants.dart';
+import 'package:emonesia/styles/app_colors.dart';
+import 'package:emonesia/styles/app_sizes.dart';
+import 'package:emonesia/styles/text_styles.dart';
 import 'package:emonesia/utils/extensions/string_extension.dart';
 import 'package:flutter/material.dart';
 
 class CustomScaffold extends StatelessWidget {
   final Widget child;
+  final String? version;
 
-  const CustomScaffold({super.key, required this.child});
+  const CustomScaffold({super.key, required this.child, this.version});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomSheet: Text(
+        AppConstants.appVersion,
+        style: TextStyles.regularOpenSansFont(
+          fontSize: AppSizes.s14,
+          color: AppColors.colorGrey,
+        ),
+      ),
       body: Stack(
         children: [
           Positioned.fill(
